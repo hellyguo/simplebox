173f494e0bd987b29f8f1db0b316dded970f69a2
Helly Guo <buffoonguo@gmail.com>
2017-06-20 15:41:28 +0800

    console : netty telnet server

--- src/main/java/com/github/hellyguo/simplebox/cmd/Commander.java
+++ src/main/java/com/github/hellyguo/simplebox/cmd/Commander.java
@@ -1,21 +1,24 @@
 package com.github.hellyguo.simplebox.cmd;
 
+import com.github.hellyguo.simplebox.cmd.netty.TelnetServer;
+import com.github.hellyguo.simplebox.cmd.netty.TelnetServerHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by Helly on 2017/06/20.
  */
 public class Commander {
-    private AtomicBoolean active = new AtomicBoolean(true);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Commander.class);
 
     public void waitForCommand() {
-        while (active.get()) {
-            try {
-                Thread.sleep(1000L);
-            } catch (InterruptedException e) {
-                //
-            }
-            break;
+        try {
+            TelnetServer server = new TelnetServer();
+            server.execute();
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage(), e);
         }
     }
 }
---
+++ src/main/java/com/github/hellyguo/simplebox/cmd/netty/TelnetServer.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.hellyguo.simplebox.cmd.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simplistic telnet server.
+ */
+public final class TelnetServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelnetServer.class);
+
+    private static final int PORT = 50023;
+
+    private int port;
+
+    public TelnetServer() {
+        this(PORT);
+    }
+
+    public TelnetServer(int port) {
+        this.port = port;
+    }
+
+    public void execute() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new TelnetServerInitializer());
+            b.bind(port).sync().channel().closeFuture().sync();
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage(), e);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
